902646910c64b1eaae803ea8212850b138366c9a
rajeshsksr <128611297+rajeshsksr@users.noreply.github.com>
2023-03-23 11:45:43 +0530

    Update JenkinsPractice.java

--- JenkinsPractice.java
+++ JenkinsPractice.java
@@ -4,6 +4,6 @@ public static void main(String[] args) {
 
 System.out.println("Hello,This is for practicing Jenkins");
 System.out.println("This happens when there code push in the Github");
-System.out.println("This is printed after code push");
+System.out.println("This is printed after code push happened in GIT");
 	}
 }
