c7ddd743188f0756284600517b7f0b145930446c
rajeshsksr <128611297+rajeshsksr@users.noreply.github.com>
2023-03-23 11:15:54 +0530

    Add files via upload
    This is after uploading java file

---
+++ JenkinsPractice.java
@@ -0,0 +1,9 @@
+public class JenkinsPractice {
+
+public static void main(String[] args) {
+
+System.out.println("Hello,This is for practicing Jenkins");
+System.out.println("This happens when there code push in the Github");
+
+	}
+}
